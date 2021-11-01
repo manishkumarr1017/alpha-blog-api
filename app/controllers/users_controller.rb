2a45c9873f800b747d580faae3ac70d23dbed040
manishkumarr1017 <manishkumarr1017@gmail.com>
2021-11-01 17:03:52 +0530

    devoloped users api

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,71 @@
+class UsersController < ApplicationController
+
+  def index
+    render json: User.all.to_json(:only => [:id, :username, :email])
+  end
+
+  def show
+    begin
+      set_user
+      render json: @user.to_json(:only => [:id, :username, :email])
+    rescue => exception
+      render json: {message: (exception.message)}
+    end
+  end
+  
+
+  def create
+    begin
+      @user = User.new(user_params)
+      if @user.save
+        render json: {jwt: "JWT token gets here"}, status: :created
+      else
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+
+    rescue => exception
+      render json: {message: "Please provide necessary details for signing up the user"}, status: :unprocessable_entity
+      
+    end
+  end
+
+  def update
+    begin
+      set_user
+      if @user.update(user_params)
+        render json: {message: "User profile successfully updated"}, status: :accepted
+      else
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+    rescue => exception
+      if exception.message.include?("Couldn't find User")
+        render json: {message: (exception.message)}
+      else
+        render json: {message: "Please provide necessary details for updating the user"}, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def destroy
+    begin
+      set_user
+      if @user.destroy
+        render json: {message: "User profile successfully got deleted"}, status: :accepted
+      end
+    rescue => exception
+      render json: {message: (exception.message)}
+    end
+  end
+  
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end 
+  
+end
